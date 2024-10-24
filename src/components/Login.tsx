85ad1fb5b69c2084c42d6bb7b6629326ee964c9f
taruavaitotta <test@student.laurea.fi>
2024-10-24 11:53:22 +0300

    Kuvaus muutoksista

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,38 @@
-import { useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
-export default function Login () {
+export default function Login() {
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+    const [error, setError] = useState<string | null>(null); // Virhetilan hallinta
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const auth = getAuth();
+
+        // Nollaa virheet ennen uutta yritystä
+        setError(null);
+
+        const email = emailRef.current?.value;
+        const password = passwordRef.current?.value;
+
+        try {
+            await signInWithEmailAndPassword(auth, email!, password!); // Kirjaudu sisään
+            // Onnistunut kirjautuminen, voit siirtyä eteenpäin, esim. redirect
+        } catch (err: any) {
+            // Käsittele virhe
+            setError(err.message); // Näytä virhe
+        }
+    };
+
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Login</h2>
-                    <Form>
+                    {error && <Alert variant="danger">{error}</Alert>} {/* Näytä virhe, jos sellainen on */}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
@@ -26,5 +49,5 @@ export default function Login () {
                 Don't have an account? Sign up!
             </div>
         </>
-    )
+    );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
